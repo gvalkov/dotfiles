2189dc5b3a4e69d714b539c0d0cbe062a4962b41
Georgi Valkov <georgi.t.valkov@gmail.com>
2018-09-02 14:45:10 +0200

    [wip]

--- i3/statusbar.py
+++ i3/statusbar.py
@@ -2,13 +2,17 @@
 # -*- coding: utf-8; -*-
 
 import os
-
 from subprocess import run
+
+import netifaces
 from i3pystatus import Status
 
 terminal = 'gnome-terminal'
 
 is_notebook = os.path.isdir('/proc/acpi/battery/')
+ifaces = netifaces.interfaces()
+
+lan1 = next(( i for i in ifaces if i.startswith('enp')), None)
 
 status = Status(interval=1)
 status.register('clock', format='%X')
@@ -20,6 +24,18 @@ if is_notebook:
 	status.register('battery', format=batfmt, alert=True, alert_percentage=10, status={'DIS': '↓', 'CHR': '↑', 'FULL': ''})
 
 status.register('pulseaudio', format='♪{volume}')
+status.register('cpu_usage_graph', format='{cpu_graph}', start_color='green', end_color='red', graph_width=7, graph_style='braille-fill')
 status.register('load', format='Load: {avg1} {avg5} {avg15}', on_leftclick=f'{terminal} -e "htop --sort-key=PERCENT_CPU"')
 status.register('mem', format='Mem: {percent_used_mem:.2f}%', on_leftclick=f'{terminal} -e "htop --sort-key=PERCENT_MEM"')
+
+status.register('disk', path='/', format=' {free}/{total}', interval=300)
+status.register('disk', path='/home/gv/', format=' {free}/{total}', on_leftclick=f'{terminal} -e "ncdu $HOME"', interval=300)
+
+if lan1:
+    status.register('network', interface=lan1,
+                    format_up=' {interface} {kbs}KB/s',
+                    format_down='wired is down',
+                    dynamic_color=False, graph_width=5,
+    )
+
 status.run()
